3bf7ecdb48b371d6f432cbfc402d847215249be0
Bill <bill@monkey.org>
2024-12-05 00:27:55 -0800

    added part 2 in a new function. keep redoing that check!

--- 05/five_a.py
+++ 05/five_a.py
@@ -22,13 +22,37 @@ def load_config(src):
 
     return dict(rules=rules, orders=orders)
 
+def fix_order(order, rules):
+    i = 1
+    while i < len(order):
+        if order[i] in rules[order[i-1]]:
+            i += 1
+            continue
+        else:
+            target = order[i-1]
+            for j in range(i, len(order)):
+                if target in rules[order[j]]:
+                    order.insert(j+1, target)
+                    order.pop(i-1)
+                    i = 1 #start over on the whole order after the fix.
+                    break
+
+    return int(order[len(order) // 2])
+
+
+
+
 def validate_and_count(config):
-    sum = 0
+    valid_sum = 0
+    fixed_sum = 0
     for order in config['orders']:
         if all(order[i] in config['rules'][order[i-1]] for i in range(1, len(order))):
-            sum += int(order[len(order) // 2])
+            valid_sum += int(order[len(order) // 2])
+        else:
+            fixed_sum += fix_order(order, config['rules'])
+            pass
 
-    return sum
+    return (valid_sum, fixed_sum)
 
 def main():
     src = 'test_input.txt'
