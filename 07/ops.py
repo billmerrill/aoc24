1e61555c4438fc858250c059f5385c68d85749a2
Bill <bill@monkey.org>
2024-12-07 17:33:22 -0800

    product, not perm

--- 07/ops.py
+++ 07/ops.py
@@ -3,22 +3,22 @@ import itertools, operator, copy
 def sum_goodies(config):
     grand_total = 0
     for c in config:
+        print(c)
         numo = len(c['operands'])-1
-        ops = [operator.add] * numo
-        ops.extend([operator.mul] * numo)
-        for attempt in itertools.permutations(ops, numo):
+        for attempt in itertools.product([operator.add, operator.mul], repeat=numo):
             nums = copy.copy(c['operands'])
             sum = nums.pop(0)
             for op in attempt:
                 sum = op(sum, nums.pop(0))
             if sum == c['result']:
                 grand_total += sum
-                print(c)
+                print('good')
                 break
     return grand_total
 
 def main():
     src = 'sample.txt'
+    src = 'input.txt'
     with open(src, 'r') as fh:
         lines = fh.readlines()
 
@@ -27,6 +27,7 @@ def main():
         result, operands = line.split(':')
         config.append({'result': int(result), 'operands': list(map(int, operands.split()))})
 
+    # sum = sum_goodies_huer1(config)
     sum = sum_goodies(config)
     print(sum)
 
